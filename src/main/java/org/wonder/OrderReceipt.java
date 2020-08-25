f7e139c9f65620fb86102c5adad0aace20d1eec0
NinputerWonder <ninputewonder@outlook.com>
2020-08-25 22:38:32 +0800

    rename the temporary variants in printReceipt method

--- src/main/java/org/wonder/OrderReceipt.java
+++ src/main/java/org/wonder/OrderReceipt.java
@@ -27,8 +27,8 @@ public class OrderReceipt {
 //        output.append(order.getCustomerLoyaltyNumber());
 
 		// prints lineItems
-		double totSalesTx = 0d;
-		double tot = 0d;
+		double totSalesTax = 0d;
+		double totalAmountOfLineItems = 0d;
 		for (LineItem lineItem : order.getLineItems()) {
 			output.append(lineItem.getDescription());
 			output.append('\t');
@@ -41,17 +41,17 @@ public class OrderReceipt {
 
 			// calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
-            totSalesTx += salesTax;
+            totSalesTax += salesTax;
 
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += lineItem.totalAmount() + salesTax;
+            totalAmountOfLineItems += lineItem.totalAmount() + salesTax;
 		}
 
 		// prints the state tax
-		output.append("Sales Tax").append('\t').append(totSalesTx);
+		output.append("Sales Tax").append('\t').append(totSalesTax);
 
         // print total amount
-		output.append("Total Amount").append('\t').append(tot);
+		output.append("Total Amount").append('\t').append(totalAmountOfLineItems);
 		return output.toString();
 	}
 }
